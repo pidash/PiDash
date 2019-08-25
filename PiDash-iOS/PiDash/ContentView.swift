00258cf787e1f6f55a4ace0063a04a2712f8a97c
Noah Peeters <noah.peeters@icloud.com>
2019-08-25 11:56:14 +0300

    Project setup

---
+++ PiDash-iOS/PiDash/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PiDash
+//
+//  Created by Noah Peeters on 25.08.19.
+//  Copyright © 2019 Noah Peeters. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
